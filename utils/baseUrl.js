d550849077ecbacbedc85e47037eb67e19c7a371
Noah Perez <nicko.perez@gmail.com>
2023-07-05 20:09:30 +0200

    base url changed

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://teza-react.envytheme.com"
+    ? "https://71a0503d.fitomag-nextjs.pages.dev"
     : "http://localhost:3000";
 
 export default baseUrl;
