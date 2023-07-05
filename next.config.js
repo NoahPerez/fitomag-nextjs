99ba381d677638270cabff4a2c2ea79bf23137ba
Noah Perez <nicko.perez@gmail.com>
2023-07-05 16:36:32 +0200

    change next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
@@ -8,10 +9,19 @@ const nextConfig = {
   },
   optimizeFonts: false,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
-}
+  },
+  webpack: (config) => {
+    // Modify the resolve configuration
+    config.resolve.extensions.push('.jsx');
 
-module.exports = nextConfig
+    // Add any additional aliases if needed
+    // For example, if you have a 'components' folder in the root directory,
+    // you can add an alias like this:
+    // config.resolve.alias['components'] = path.resolve(__dirname, 'components');
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
