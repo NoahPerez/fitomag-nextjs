19f57f28fa6a443bbb8bcf691c728490a3d0f30f
Noah Perez <nicko.perez@gmail.com>
2023-07-05 20:29:27 +0200

    base url changed again

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://71a0503d.fitomag-nextjs.pages.dev"
+    ? "https://fitomag-nextjs.pages.dev"
     : "http://localhost:3000";
 
 export default baseUrl;
