2fc01f6c9ba45dfd8515a1548cd7072e90b40aee
Noah Perez <nicko.perez@gmail.com>
2023-07-11 14:34:28 +0200

    change baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://site--strapi--6vkv6s4mwd2c.code.run"
+    ? "https://21416f71.fitomag-nextjs.pages.dev"
     : "http://localhost:3000";
 
 export default baseUrl;
