d8ec6ab8db0bff83a53c525842e12f0547614d36
Noah Perez <nicko.perez@gmail.com>
2023-07-11 14:22:56 +0200

    change baseApiUrl

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+
+import axios from 'axios';
+
+export async function fetcher(url, options = {}) {
+  try {
+    const response = await axios(url, options);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    // Handle error
+    console.error('Request failed:', error.message);
+    throw error;
+  }
 }
