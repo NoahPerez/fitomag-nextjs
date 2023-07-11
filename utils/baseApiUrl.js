4f20cce7929471e13366f18ead9dd420f0e90fcf
Noah Perez <nicko.perez@gmail.com>
2023-07-11 13:57:40 +0200

    change baseApiUrl

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,5 +1,5 @@
 const baseApiUrl = process.env.NODE_ENV === "production" 
-? 'https://site--strapi--6vkv6s4mwd2c.code.run/api'
+? 'https://site--strapi--6vkv6s4mwd2c.code.run'
 : 'http://localhost:1337';
 
 export default baseApiUrl;
