dd85d8e79dd9cf6c48ab45a7c02b69dd4ebc8d86
Noah Perez <nicko.perez@gmail.com>
2023-07-12 13:39:58 +0200

    add api

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://fitomag-nextjs.pages.dev/api"
+    ? "https://fitomag-nextjs.pages.dev"
     : "http://localhost:3000";
 
 export default baseUrl;
