8c85789271dc5565f308f2cb58b7d73e113cc8a5
Noah Perez <nicko.perez@gmail.com>
2023-07-13 15:10:52 +0200

    add axious fetch

--- pages/api/api.js
+++ pages/api/api.js
@@ -19,23 +19,23 @@ export async function fetcher(url, options = {}) {
   }
 }
 
-// const headers = {
-//   'Content-Type': 'application/json',
-//   'Authorization': '1604ebb038b4c193dd3d787e5a668143657b8edbe2df198d453f6c6e6fe31476dff5a92fdf70ca8d9ae629e5d6bf430539eb72b280ea7c1c6a35c2dd42282ddd1c661b378c68852ffbabf6739636b02153b8b4a64185d12273a6a3bde3ac7d0edff1e6b8d7b7b7607a4cc21104b843a0b8496cdd744d0e4edf70aad482c35327', // This is where the API key is used
-// };
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': '1604ebb038b4c193dd3d787e5a668143657b8edbe2df198d453f6c6e6fe31476dff5a92fdf70ca8d9ae629e5d6bf430539eb72b280ea7c1c6a35c2dd42282ddd1c661b378c68852ffbabf6739636b02153b8b4a64185d12273a6a3bde3ac7d0edff1e6b8d7b7b7607a4cc21104b843a0b8496cdd744d0e4edf70aad482c35327', // This is where the API key is used
+};
 
-// // Make the API request using the configured headers
-// fetch('https://site--strapi--6vkv6s4mwd2c.code.run/data', {
-//   method: 'GET',
-//   headers: headers,
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // Handle the response data
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     // Handle any errors
-//     console.error(error);
-//   });
+// Make the API request using the configured headers
+fetch('https://site--strapi--6vkv6s4mwd2c.code.run/data', {
+  method: 'GET',
+  headers: headers,
+})
+  .then((response) => response.json())
+  .then((data) => {
+    // Handle the response data
+    console.log(data);
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.error(error);
+  });
 
